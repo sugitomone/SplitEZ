c564ded74a54a073b69e5ce112aea91a943c0cf4
sugitomone <127827194+sugitomone@users.noreply.github.com>
2023-10-27 21:19:10 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 document.getElementById("calculateButton").addEventListener("click", function () {
-  const total = parseFloat(document.getElementById("total").value);
   const person1 = document.getElementById("person1").value;
   const amount1 = parseFloat(document.getElementById("amount1").value);
   const person2 = document.getElementById("person2").value;
   const amount2 = parseFloat(document.getElementById("amount2").value);
 
-  if (isNaN(total) || isNaN(amount1) || isNaN(amount2)) {
-    document.getElementById("results").textContent = "正しい数値を入力してください。";
+  if (isNaN(amount1) || isNaN(amount2)) {
+    document.getElementById("results").textContent = "正しい金額を入力してください。";
   } else {
-    const share1 = (amount1 / (amount1 + amount2)) * total;
-    const share2 = (amount2 / (amount1 + amount2)) * total;
+    const share1 = (amount1 / (amount1 + amount2)).toFixed(2);
+    const share2 = (amount2 / (amount1 + amount2)).toFixed(2);
 
-    const resultText = `${person1} は ${share1.toFixed(2)} を支払うべきで、${person2} は ${share2.toFixed(2)} を支払うべきです。`;
+    const resultText = `${person1} は ${share1} を支払うべきで、${person2} は ${share2} を支払うべきです。`;
     document.getElementById("results").textContent = resultText;
   }
 });
